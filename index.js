6da4275a80bced311ac9ad118b5a95fc2d46c479
Evellyn <xe.uyemura@gmail.com>
2020-07-06 15:14:23 -0300

    adicionando comandos

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const mdLinks = require('./main');
+#!/usr/bin/env node //Interpretador de node para outros sistemas operacionais.
 
-console.log(mdLinks());
+const program = require('commander'); /** Cria comandos na CLI */
+const pack = require('./package.json'); /** Arquivo package json */
+
+program.version(pack.version); /** Verifica a versão do package Json */
+
+program
+  .command('init [mdlinks]')
+  .description('Verifica links da web')
+  .action((mdlinks) => {
+    console.log(mdlinks);
+  });
+
+program.parse(process.argv); /** Interpreta comandos do Node.js */
