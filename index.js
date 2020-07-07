19aea546b4f70969c4237df279f87810649b17ca
Evellyn <xe.uyemura@gmail.com>
2020-07-07 20:50:56 -0300

    validando links

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const { join } = require('path'); /** Pega a url do arquivo */
 const fs = require('fs'); /** Lê o arquivo */
 const chalk = require('chalk'); /** Adiciona cor. */
 const Table = require('cli-table'); /* Cria uma nova tabela */
+const axios = require('axios'); /** Faz requisições HTTP */
 const program = require('commander'); /** Criar um comando no console */
 
 const pack = require('./package.json'); /** Pegar o arquivo package JSON */
 
-const paths = join(__dirname, 'README.md'); /** Cria um caminho. */
+const paths = join(__dirname, 'teste.md'); /** Cria um caminho. */
 
-program.version(pack.version);
+program.version(pack.version); /** Verifica a versão do package JSON */
 
 const getJson = (path) => {
   const data = fs.existsSync(path) ? fs.readFileSync(path, 'utf8') : [];
@@ -38,43 +39,68 @@ const showTable = (data, validate) => {
   if (validate === true) {
     const table = new Table({
       head: ['id', 'texto', 'link', 'status'],
-      colWidths: [5, 50, 100, 5],
+      colWidths: [5, 50, 120, 20],
     });
     data.forEach((e, index) => {
       table.push([
         index,
         e.text,
-        chalk.green(e.link),
-        e.status === 200 ? chalk.green(e.status) : chalk.red(e.status),
+        chalk.cyan(e.link),
+        e.stats === 200 ? chalk.green(e.stats) : chalk.red(e.stats),
       ]);
     });
     console.log(table.toString());
   } else {
     const table = new Table({
       head: ['id', 'texto', 'link'],
-      colWidths: [5, 100, 100],
+      colWidths: [5, 50, 120],
     });
     data.forEach((e, index) => {
       table.push([
         index,
         e.text,
-        chalk.green(e.link),
+        chalk.cyan(e.link),
       ]);
     });
     console.log(table.toString());
   }
 };
 
+const validateLink = (data) => {
+  data.forEach((e) => {
+    axios.get(e.link)
+      .then((response) => {
+        console.log(`
+          ${chalk.green(e.text)}
+          ${chalk.green(e.link)}
+          ${chalk.green(response.status)}
+        `);
+      })
+      .catch(() => {
+        console.log(`
+          ${chalk.red(e.text)}
+          ${chalk.red(e.link)}
+          ${chalk.red(404)}
+        `);
+      });
+  });
+};
+
 program
   .command('add [mdlinks]')
   .description('Verify a link')
   .option('-v, --validate [verify]', 'sendo true verifica se a url é valida ou não')
-  .option('-s, --status [status]', 'sendo true mostra quantos links são válidos')
+  .option('-s, --stats [stats]', 'sendo true mostra quantos links são válidos')
   .action((mdlinks, options) => {
-    const { validate, status } = options;
-    console.log(validate, status);
-    const data = getJson(paths);
-    showTable(data, validate);
+    const { validate, stats } = options; /** Pega validate e stats dentro de options */
+    console.log(stats);
+    const data = getJson(paths); /** Pega os links do arquivo selecionado. */
+    if (validate === true) {
+      validateLink(data);
+    } else {
+      showTable(data, validate);
+      console.log(`${chalk.green('Fim do processo')}`);
+    }
   });
 
-program.parse(process.argv);
+program.parse(process.argv); /** Interpretador do Node.js */
---
+++ teste.md
@@ -0,0 +1,4 @@
+#Teste
+[Google](https://www.google.com/)
+[Facebook](https://www.facebook.com/)
+[Alura](https://www.aalura.com.br/)
